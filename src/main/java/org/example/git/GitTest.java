6e3f623db0e7fdc1f5cbf729eed7ee7f46f2b997
yys <2814287161@qq.com>
2023-07-16 02:56:48 +0800

    master commit1

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -8,6 +8,7 @@ package org.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("master update");
     }
 
     public static void print() {
