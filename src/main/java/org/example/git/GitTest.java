d89ef592c1efb7424d5ab6e37004a081207f0372
yys <2814287161@qq.com>
2023-07-16 02:59:14 +0800

    dev commit2

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("dev update");
+        System.out.println("dev");
     }
 
     public static void print() {
